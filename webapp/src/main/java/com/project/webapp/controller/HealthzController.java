b11309d899c167cdafc66cd282600c83d25a4e6c
Mani Charan <loka.m@northeastern.edu>
2023-10-03 00:41:22 -0400

    added assignment 2

---
+++ webapp/src/main/java/com/project/webapp/controller/HealthzController.java
@@ -0,0 +1,38 @@
+package com.project.webapp.controller;
+
+import com.project.webapp.service.DatabaseService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HealthzController {
+    @Autowired
+    DatabaseService databaseService;
+
+    @GetMapping("/healthz")
+    public ResponseEntity<String> getHealth(@RequestBody(required = false) String requestBody, HttpServletRequest request){
+        if(requestBody!=null || !request.getParameterMap().isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).header("BadRequest","Invalid Request").cacheControl(CacheControl.noCache()).build();
+        }
+        if(databaseService.isServerRunning()){
+            return ResponseEntity.ok().header("OK","Success").cacheControl(CacheControl.noCache()).build();
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).header("Unavailable","The Server Cannot Handle The Request").cacheControl(CacheControl.noCache()).build();
+        }
+    }
+
+    //any methods other than GET for the same endpoint
+    @RequestMapping(value="/healthz",method = {RequestMethod.DELETE,RequestMethod.POST,RequestMethod.PUT,RequestMethod.PATCH,RequestMethod.HEAD,RequestMethod.OPTIONS})
+    public ResponseEntity<String> anyOtherMethod(){
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).header("NotAllowed","The Requested Method Is Not Allowed").cacheControl(CacheControl.noCache()).build();
+    }
+    @RequestMapping(value ="/**",method = {RequestMethod.GET,RequestMethod.DELETE,RequestMethod.POST,RequestMethod.PUT,RequestMethod.PATCH,RequestMethod.HEAD,RequestMethod.OPTIONS})
+    public ResponseEntity<String> anyOtherUrl(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).header("NotFound","The Requested Resource Is Not Found").cacheControl(CacheControl.noCache()).build();
+    }
+}
---
+++ webapp/src/main/java/com/project/webapp/service/DatabaseService.java
@@ -0,0 +1,26 @@
+package com.project.webapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Service
+public class DatabaseService {
+    DataSource dataSource;
+    @Autowired
+    public DatabaseService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    public boolean isServerRunning() {
+        try{
+            Connection connection = dataSource.getConnection();
+            return true;
+        }
+        catch (SQLException e){
+            return false;
+        }
+    }
+}
