6d5fe8b80b4bb59242848342384c0b43b57651fa
Mani Charan <loka.m@northeastern.edu>
2023-10-03 06:17:33 -0400

    testing CI

--- src/main/java/com/project/webapp/controller/AssignmentController.java
+++ src/main/java/com/project/webapp/controller/AssignmentController.java
@@ -1,4 +1,9 @@
 package com.project.webapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AssignmentController {
+
 }
