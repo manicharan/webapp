998199580f97bf5cb29738297cd39a1be76a6ef3
Mani Charan <loka.m@northeastern.edu>
2023-10-03 21:49:20 -0400

    added authentication logic

--- src/main/java/com/project/webapp/config/SecurityConfig.java
+++ src/main/java/com/project/webapp/config/SecurityConfig.java
@@ -1,15 +1,31 @@
 package com.project.webapp.config;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/healthz","GET")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/v1/assignments/*","GET")).permitAll()
+                        .anyRequest()
+                        .authenticated());
+        http.httpBasic();
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(10);
