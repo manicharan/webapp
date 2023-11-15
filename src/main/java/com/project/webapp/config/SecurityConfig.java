c1e8d38de214c0524858581db4225e152b04f51c
Mani Charan <loka.m@northeastern.edu>
2023-11-15 11:22:51 -0500

    added jmeter script and made minor config changes

--- src/main/java/com/project/webapp/config/SecurityConfig.java
+++ src/main/java/com/project/webapp/config/SecurityConfig.java
@@ -1,18 +1,15 @@
 package com.project.webapp.config;
+
 import com.project.webapp.service.DatabaseService;
-import com.project.webapp.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -26,6 +23,8 @@ public class SecurityConfig{
     private CustomAuthenticationProvider customAuthenticationProvider;
     @Autowired
     private DatabaseService databaseService;
+    private static final Logger logger = LogManager.getLogger(SecurityConfig.class);
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
@@ -47,6 +46,7 @@ public class SecurityConfig{
             if (!databaseService.isServerRunning()) {
                 response.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
             } else {
+                logger.warn("Authorization failed");
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             }
         };
