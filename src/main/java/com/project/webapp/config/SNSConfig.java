6a033522f91d938946c1bb8892fb623ab93e144d
Mani Charan <loka.m@northeastern.edu>
2023-11-28 02:48:37 -0500

    fixing integration test failure

--- src/main/java/com/project/webapp/config/SNSConfig.java
+++ src/main/java/com/project/webapp/config/SNSConfig.java
@@ -1,6 +1,5 @@
 package com.project.webapp.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
