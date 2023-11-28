7f59e9fca16cce147c20336a554d462ff898dbef
Mani Charan <loka.m@northeastern.edu>
2023-11-28 02:29:31 -0500

    fixing integration test failure

--- src/main/java/com/project/webapp/config/SNSConfig.java
+++ src/main/java/com/project/webapp/config/SNSConfig.java
@@ -2,19 +2,20 @@ package com.project.webapp.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 @Component
-public class TestConfig {
+public class SNSConfig {
+    @Value("${aws.region}")
+    private String region;
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(10);
+    public SnsClient snsClient() {
+        return SnsClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
     }
 }
