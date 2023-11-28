e0db2c8cd6fb25f46f5f041e458968537df35a8d
Mani Charan <loka.m@northeastern.edu>
2023-11-28 02:36:03 -0500

    fixing integration test failure

--- src/main/java/com/project/webapp/config/TestConfig.java
+++ src/main/java/com/project/webapp/config/TestConfig.java
@@ -1,15 +1,9 @@
 package com.project.webapp.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sns.SnsClient;
 
 @Component
 public class TestConfig {
